8e9f95be038b08877b651d5062844995015e06b2
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-29 17:10:47 +0200

    Blank sign-in component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from "./shared/material/material.module";
 
 import {AppComponent} from './app.component';
+import {SignInComponent} from './views/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/sign-in.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'ae-sign-in',
+  template: `
+    <p>
+      sign-in works!
+    </p>
+  `,
+  styles: []
+})
+export class SignInComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
