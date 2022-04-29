34309bff6aed2c7d20fb3d1857ffe8969dfa0f8a
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-29 17:40:16 +0200

    Started debugging component.

--- src/app/features/debug/debug.component.ts
+++ src/app/features/debug/debug.component.ts
@@ -3,9 +3,11 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'ae-debug',
   template: `
-    <p>
-      debug works!
-    </p>
+    <div style="width: 992px; margin: 20px auto; border: 3px solid red; padding: 20px;">
+      <h2>debugging...</h2>
+
+      <!--call your component here-->
+    </div>
   `,
   styles: []
 })
--- src/app/views/sign-in.component.ts
+++ src/app/views/sign-in.component.ts
@@ -1,13 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 
+/**
+ * Based from Angular Material 13.3.5 and component "Input".
+ * See "Basic Inputs" from the relative examples (check the docs).
+ *
+ * @link https://material.angular.io/components/input/examples
+ */
 @Component({
   selector: 'ae-sign-in',
   template: `
-    <p>
-      sign-in works!
-    </p>
+    <form class="example-form">
+      <mat-form-field class="example-full-width" appearance="fill">
+        <mat-label>Favorite food</mat-label>
+        <input matInput placeholder="Ex. Pizza" value="Sushi">
+      </mat-form-field>
+
+      <mat-form-field class="example-full-width" appearance="fill">
+        <mat-label>Leave a comment</mat-label>
+        <textarea matInput placeholder="Ex. It makes me feel..."></textarea>
+      </mat-form-field>
+    </form>
   `,
-  styles: []
+  styles: [`
+    .example-form {
+      min-width: 150px;
+      max-width: 500px;
+      width: 100%;
+    }
+
+    .example-full-width {
+      width: 100%;
+    }
+  `]
 })
 export class SignInComponent implements OnInit {
 
