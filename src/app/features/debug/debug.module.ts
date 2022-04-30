3c675fd58e2e3445689a663b988a0ff5b9b043b7
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-30 15:13:23 +0200

    Added default address-form.

--- src/app/features/debug/debug.module.ts
+++ src/app/features/debug/debug.module.ts
@@ -2,28 +2,32 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DebugRoutingModule} from './debug-routing.module';
 import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SharedModule} from "../../shared/shared.module";
 import {MaterialModule} from "../../shared/material/material.module";
 
 import {DebugComponent} from './debug.component';
 import {SignInComponent} from "../../views/sign-in.component";
 import {RegisterComponent} from '../../views/register.component';
+import {AddressFormComponent} from '../../views/address-form/address-form.component';
 
 @NgModule({
   declarations: [
     DebugComponent,
     SignInComponent,
-    RegisterComponent
+    RegisterComponent,
+    AddressFormComponent
   ],
   imports: [
     CommonModule,
     DebugRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     MaterialModule
-  ]
+  ],
+  exports: []
 })
 export class DebugModule {
 }
