3383065378ff67fe3a92c4f4b18b329999ddaad4
ailequal <37016865+ailequal@users.noreply.github.com>
2022-05-16 15:55:49 +0200

    Updating basic components styles.

--- src/app/views/register.component.ts
+++ src/app/views/register.component.ts
@@ -10,119 +10,143 @@ import {NgForm} from "@angular/forms";
 @Component({
   selector: 'ae-register',
   template: `
-    <form #f="ngForm" (ngSubmit)="submitHandler(f)" class="register-form">
-      <mat-form-field class="full-width" appearance="fill">
-        <mat-label>Email</mat-label>
-        <input
-          ngModel
-          #nameRef="ngModel"
-          name="email"
-          matInput
-          type="email"
-          required
-          email
-          placeholder="lucastip@earth.org"
-        >
-        <mat-error *ngIf="nameRef.errors?.['email']">
-          Inserire un indirizzo email valido.
-        </mat-error>
-        <mat-error *ngIf="nameRef.errors?.['required']">
-          Il campo email è <strong>obbligatorio</strong>.
-        </mat-error>
-      </mat-form-field>
+    <form #f="ngForm" (ngSubmit)="submitHandler(f)">
+      <mat-card class="register">
 
-      <!--debut input status-->
-      <pre>
-      {{nameRef.errors | json}}
-      </pre>
+        <mat-card-header>
+          <mat-card-title>Register</mat-card-title>
+        </mat-card-header>
 
-      <mat-form-field class="full-width" appearance="fill">
-        <mat-label>Nome</mat-label>
-        <input
-          ngModel
-          name="name"
-          matInput
-          type="text"
-          required
-          minlength="3"
-          maxlength="24"
-          placeholder="Lucas"
-        >
-      </mat-form-field>
+        <mat-card-content>
+          <mat-form-field class="full-width" appearance="fill">
+            <mat-label>Email</mat-label>
+            <input
+              ngModel
+              #nameRef="ngModel"
+              name="email"
+              matInput
+              type="email"
+              required
+              email
+              placeholder="lucastip@earth.org"
+            >
+            <mat-error *ngIf="nameRef.errors?.['email']">
+              Inserire un indirizzo email valido.
+            </mat-error>
+            <mat-error *ngIf="nameRef.errors?.['required']">
+              Il campo email è <strong>obbligatorio</strong>.
+            </mat-error>
+          </mat-form-field>
 
-      <mat-form-field class="full-width" appearance="fill">
-        <mat-label>Cognome</mat-label>
-        <input
-          ngModel
-          name="surname"
-          matInput
-          type="text"
-          required
-          minlength="3"
-          maxlength="24"
-          placeholder="Tip"
-        >
-      </mat-form-field>
+          <!--debut input status-->
+          <pre>
+            {{nameRef.errors | json}}
+          </pre>
 
-      <mat-form-field class="full-width" appearance="fill">
-        <mat-label>Password</mat-label>
-        <input
-          ngModel
-          name="passwordAlpha"
-          matInput
-          [type]="hideAlpha ? 'password': 'text'"
-          required
-          minlength="8"
-          maxlength="64"
-        >
-        <mat-icon matSuffix (click)="hideAlpha = !hideAlpha">{{hideAlpha ? 'visibility_off' : 'visibility'}}</mat-icon>
-      </mat-form-field>
+          <mat-form-field class="full-width" appearance="fill">
+            <mat-label>Nome</mat-label>
+            <input
+              ngModel
+              name="name"
+              matInput
+              type="text"
+              required
+              minlength="3"
+              maxlength="24"
+              placeholder="Lucas"
+            >
+          </mat-form-field>
 
-      <mat-form-field class="full-width" appearance="fill">
-        <mat-label>Ripeti la password</mat-label>
-        <input
-          ngModel
-          name="passwordBeta"
-          matInput
-          [type]="hideBeta ? 'password': 'text'"
-          required
-          minlength="8"
-          maxlength="64"
-        >
-        <mat-icon matSuffix (click)="hideBeta = !hideBeta">{{hideBeta ? 'visibility_off' : 'visibility'}}</mat-icon>
-      </mat-form-field>
+          <mat-form-field class="full-width" appearance="fill">
+            <mat-label>Cognome</mat-label>
+            <input
+              ngModel
+              name="surname"
+              matInput
+              type="text"
+              required
+              minlength="3"
+              maxlength="24"
+              placeholder="Tip"
+            >
+          </mat-form-field>
 
-      <button
-        mat-raised-button
-        type="submit"
-        [disabled]="f.invalid"
-        class="full-width mb"
-        color="primary"
-      >
-        Registrati
-      </button>
+          <mat-form-field class="full-width" appearance="fill">
+            <mat-label>Password</mat-label>
+            <input
+              ngModel
+              name="passwordAlpha"
+              matInput
+              [type]="hideAlpha ? 'password': 'text'"
+              required
+              minlength="8"
+              maxlength="64"
+            >
+            <mat-icon matSuffix
+                      (click)="hideAlpha = !hideAlpha">{{hideAlpha ? 'visibility_off' : 'visibility'}}</mat-icon>
+          </mat-form-field>
 
-      <button
-        mat-raised-button
-        type="button"
-        class="full-width"
-      >
-        Hai già un account? Accedi
-      </button>
+          <mat-form-field class="full-width" appearance="fill">
+            <mat-label>Ripeti la password</mat-label>
+            <input
+              ngModel
+              name="passwordBeta"
+              matInput
+              [type]="hideBeta ? 'password': 'text'"
+              required
+              minlength="8"
+              maxlength="64"
+            >
+            <mat-icon matSuffix (click)="hideBeta = !hideBeta">{{hideBeta ? 'visibility_off' : 'visibility'}}</mat-icon>
+          </mat-form-field>
+        </mat-card-content>
+
+        <mat-card-actions>
+          <button
+            mat-raised-button
+            type="submit"
+            [disabled]="f.invalid"
+            class="full-width mb"
+            color="primary"
+          >
+            Registrati
+          </button>
+
+          <button
+            mat-raised-button
+            type="button"
+            class="full-width"
+          >
+            Hai già un account? Accedi
+          </button>
+        </mat-card-actions>
+
+      </mat-card>
     </form>
   `,
   styles: [`
-    .register-form {
-      min-width: 150px;
-      max-width: 500px;
-      width: 100%;
+    .register {
+      min-width: 120px;
+      margin: 20px auto;
     }
 
     .full-width {
       width: 100%;
     }
 
-    .mb {
+    .mat-card-header {
+      justify-content: center;
+    }
+
+    .mat-card-actions .mat-button,
+    .mat-card-actions .mat-raised-button,
+    .mat-card-actions .mat-stroked-button {
+      margin: 0 0 0 0;
+    }
+
+    .mat-card-actions .mat-button.mb,
+    .mat-card-actions .mat-raised-button.mb,
+    .mat-card-actions .mat-stroked-button.mb {
       margin-bottom: 20px;
     }
   `]
--- src/app/views/sign-in.component.ts
+++ src/app/views/sign-in.component.ts
@@ -10,67 +10,90 @@ import {NgForm} from "@angular/forms";
 @Component({
   selector: 'ae-sign-in',
   template: `
-    <form #f="ngForm" (ngSubmit)="submitHandler(f)" class="sign-in-form">
-      <mat-form-field class="full-width" appearance="fill">
-        <mat-icon matPrefix color="primary" aria-hidden="false" aria-label="Person icon">person</mat-icon>
-        <mat-label>Email</mat-label>
-        <input
-          ngModel
-          name="email"
-          matInput
-          type="email"
-          required
-          email
-          placeholder="lucastip@earth.org"
-        >
-      </mat-form-field>
+    <form #f="ngForm" (ngSubmit)="submitHandler(f)">
+      <mat-card class="sign-in">
 
-      <mat-form-field class="full-width" appearance="fill">
-        <mat-icon matPrefix color="primary" aria-hidden="false" aria-label="Lock icon">lock</mat-icon>
-        <mat-label>Password</mat-label>
-        <input
-          ngModel
-          name="password"
-          matInput
-          [type]="hide ? 'password': 'text'"
-          required
-          minlength="8"
-          maxlength="64"
-        >
-        <mat-icon matSuffix (click)="hide = !hide">{{hide ? 'visibility_off' : 'visibility'}}</mat-icon>
-      </mat-form-field>
+        <mat-card-header>
+          <mat-card-title>Sign In</mat-card-title>
+        </mat-card-header>
 
-      <button
-        mat-raised-button
-        type="submit"
-        [disabled]="f.invalid"
-        class="full-width mb"
-        color="primary"
-      >
-        Accedi
-      </button>
+        <mat-card-content>
+          <mat-form-field class="full-width" appearance="fill">
+            <mat-icon matPrefix color="primary" aria-hidden="false" aria-label="Person icon">person</mat-icon>
+            <mat-label>Email</mat-label>
+            <input
+              ngModel
+              name="email"
+              matInput
+              type="email"
+              required
+              email
+              placeholder="lucastip@earth.org"
+            >
+          </mat-form-field>
 
-      <button
-        mat-raised-button
-        type="button"
-        class="full-width"
-      >
-        Crea un nuovo account
-      </button>
+          <mat-form-field class="full-width" appearance="fill">
+            <mat-icon matPrefix color="primary" aria-hidden="false" aria-label="Lock icon">lock</mat-icon>
+            <mat-label>Password</mat-label>
+            <input
+              ngModel
+              name="password"
+              matInput
+              [type]="hide ? 'password': 'text'"
+              required
+              minlength="8"
+              maxlength="64"
+            >
+            <mat-icon matSuffix (click)="hide = !hide">{{hide ? 'visibility_off' : 'visibility'}}</mat-icon>
+          </mat-form-field>
+        </mat-card-content>
+
+        <mat-card-actions>
+          <button
+            mat-raised-button
+            type="submit"
+            [disabled]="f.invalid"
+            class="full-width mb"
+            color="primary"
+          >
+            Accedi
+          </button>
+
+          <button
+            mat-raised-button
+            type="button"
+            class="full-width"
+          >
+            Crea un nuovo account
+          </button>
+        </mat-card-actions>
+
+      </mat-card>
     </form>
   `,
   styles: [`
-    .sign-in-form {
-      min-width: 150px;
-      max-width: 500px;
-      width: 100%;
+    .sign-in {
+      min-width: 120px;
+      margin: 20px auto;
     }
 
     .full-width {
       width: 100%;
     }
 
-    .mb {
+    .mat-card-header {
+      justify-content: center;
+    }
+
+    .mat-card-actions .mat-button,
+    .mat-card-actions .mat-raised-button,
+    .mat-card-actions .mat-stroked-button {
+      margin: 0 0 0 0;
+    }
+
+    .mat-card-actions .mat-button.mb,
+    .mat-card-actions .mat-raised-button.mb,
+    .mat-card-actions .mat-stroked-button.mb {
       margin-bottom: 20px;
     }
   `]
