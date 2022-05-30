a8332731270c669ce8750f974ffe732be23cfdce
ailequal <37016865+ailequal@users.noreply.github.com>
2022-05-30 17:10:43 +0200

    Added italian locale setting.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {registerLocaleData} from "@angular/common";
+import it from "@angular/common/locales/it";
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from "./shared/material/material.module";
 
 import {AppComponent} from './app.component';
+
+registerLocaleData(it)
 
 @NgModule({
   declarations: [
@@ -16,7 +20,7 @@ import {AppComponent} from './app.component';
     BrowserAnimationsModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'it'}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
