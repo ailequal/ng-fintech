9046a334b91cb328bb119454bf0b309112418c4c
ailequal <37016865+ailequal@users.noreply.github.com>
2022-05-30 15:01:00 +0200

    First styling for the movements component.

--- src/app/views/movements.component.ts
+++ src/app/views/movements.component.ts
@@ -3,9 +3,24 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'ae-movements',
   template: `
-    <p>
-      movements works!
-    </p>
+    <div>
+      <mat-form-field appearance="fill">
+        <mat-label>Seleziona una carta</mat-label>
+        <mat-select>
+          <mat-option [value]="'card-01'">Carta 1</mat-option>
+          <mat-option [value]="'card-02'">Carta 2</mat-option>
+          <mat-option [value]="'card-03'">Carta 3</mat-option>
+        </mat-select>
+      </mat-form-field>
+
+      <h2>Saldo: {{2000 | currency: 'EUR'}}</h2>
+
+      <div>
+        list of movements
+      </div>
+
+      <button>Carica altro</button>
+    </div>
   `,
   styles: []
 })
