a4cf3718ed85fb2db10b5e4f6561ace35df570e9
ailequal <37016865+ailequal@users.noreply.github.com>
2022-05-30 22:31:32 +0200

    Working on movements select.

--- src/app/views/movements.component.ts
+++ src/app/views/movements.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Card} from "../model/card";
 import {Movement, MovementType} from "../model/movement";
+import {MatSelectChange} from "@angular/material/select";
 
 @Component({
   selector: 'ae-movements',
@@ -8,18 +9,18 @@ import {Movement, MovementType} from "../model/movement";
     <div>
       <mat-form-field appearance="fill">
         <mat-label>Seleziona una carta</mat-label>
-        <mat-select>
+        <mat-select (selectionChange)="onSelectionChange($event)">
           <mat-option *ngFor="let card of cards" [value]="card._id">
             {{card.number}}
           </mat-option>
         </mat-select>
       </mat-form-field>
 
-      <h2>Saldo: {{2000 | currency: 'EUR'}}</h2>
+      <h2>Saldo: {{selectedCard?.amount | currency: 'EUR'}}</h2>
 
-      <div>
+      <div *ngIf="selectedCard" class="movements">
         <ae-movement
-          *ngFor="let movement of movements"
+          *ngFor="let movement of selectedMovements"
           [title]="movement.title"
           [description]="movement.description"
           [amount]=movement.amount
@@ -61,31 +62,73 @@ export class MovementsComponent implements OnInit {
     },
   ];
 
-  movements: Movement[] = [
+  selectedCard: Card | null = null
+
+  movements: { id: string, movements: Movement[] }[] = [
     {
-      _id: '437264958372',
-      title: 'Biglietto aereo Milano-Parigi',
-      description: 'Comprato un biglietto aereo per Parigi da Milano.',
-      cardId: '347987294424',
-      amount: 200,
-      type: 'out',
-      timestamp: 1643923659000
-    },
-    {
-      _id: '437264958373',
-      title: 'Spesa presso Despar',
-      description: 'Comprato la spesa della settimana al Despar.',
-      cardId: '347987294424',
-      amount: 160,
-      type: 'out',
-      timestamp: 1642923659000
-    },
+      id: '347987294424',
+      movements: [
+        {
+          _id: '437264958372',
+          title: 'Biglietto aereo Milano-Parigi',
+          description: 'Comprato un biglietto aereo per Parigi da Milano.',
+          cardId: '347987294424',
+          amount: 200,
+          type: 'out',
+          timestamp: 1643923659000
+        },
+        {
+          _id: '437264958373',
+          title: 'Spesa presso Despar',
+          description: 'Comprato la spesa della settimana al Despar.',
+          cardId: '347987294424',
+          amount: 160,
+          type: 'out',
+          timestamp: 1642923659000
+        }
+      ]
+    }
   ]
+
+  selectedMovements: Movement[] = []
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  onSelectionChange(event: MatSelectChange) {
+    if (!event.value) {
+      this.selectedCard = null;
+      this.selectedMovements = [];
+      return;
+    }
+
+    // Set the active card.
+    const selectedCard = this.cards.find(element => {
+      return element._id === event.value
+    })
+
+    if (!selectedCard) {
+      this.selectedCard = null;
+      this.selectedMovements = []
+      return;
+    }
+
+    this.selectedCard = selectedCard
+
+    // Set the active movements.
+    const selectedMovements = this.movements.find(element => {
+      return element.id === this.selectedCard?._id
+    })
+
+    if (!selectedMovements) {
+      this.selectedMovements = []
+      return;
+    }
+
+    this.selectedMovements = selectedMovements.movements
   }
 
 }
