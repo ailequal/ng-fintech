e1f43688217e48ff6618183d070447f8c906d929
Elia Pari <37016865+ailequal@users.noreply.github.com>
2022-08-12 12:21:25 +0200

    added ngrx initial state setup with schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StoreModule} from '@ngrx/store';
+import {reducers, metaReducers} from './reducers';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 import {SharedModule} from "./shared/shared.module";
 
 import {AppComponent} from './app.component';
@@ -20,6 +24,8 @@ registerLocaleData(it)
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
     SharedModule
   ],
   providers: [{provide: LOCALE_ID, useValue: 'it'}],
